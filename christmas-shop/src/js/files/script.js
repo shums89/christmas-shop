c76d57da0957e3cc59508817b24d4377553286a7
Aleksandr Shumatbaev <sh889@ya.ru>
2024-12-01 14:38:51 +0500

    fix: functionality menu link

--- christmas-shop/src/js/files/script.js
+++ christmas-shop/src/js/files/script.js
@@ -3,7 +3,9 @@ import { menuClose } from "./functions.js";
 const links = document.querySelectorAll('.menu__link');
 
 links.forEach(link => link.addEventListener('click', e => {
-  if (e.target.closest('.html-menu') && e.target.closest('.menu__link_menu')) {
+  console.log(e.target);
+
+  if (e.target.closest('.html-gifts') && e.target.closest('.menu__link_gifts')) {
     e.preventDefault();
   }
 
